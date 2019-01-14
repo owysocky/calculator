937dc33ec46b2fb9740d536992125e14fc2231a0
Olha Wysocky and Pavel Zanchuk <pavel@>
2019-01-14 11:17:57 -0800

    add README.md

---
+++ README.md
@@ -0,0 +1,23 @@
+# _Calculator_
+
+#### _Calculator application 01/11/2019_
+
+#### By _**Olha Wysocky and Pavel Zanchuk**_
+
+## Description
+
+_This is a calculator application to practice and learn how to use JS_
+
+
+## Technologies Used
+
+* _HTML_
+* _CSS_
+* _ATOM_
+* _JS_
+
+### License
+MIT
+**
+
+Copyright (c) 2019 **_Olha Wysocky and Pavel Zanchuk_**
--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,19 @@ function add(number1, number2) {
 return number1 + number2;
 };
 
+function subtract(number1, number2) {
+return number1 - number2;
+};
+
+function mult(number1, number2) {
+return number1 * number2;
+};
+
+function divide(number1, number2) {
+return number1 / number2;
+};
+
 var number1 = parseInt(prompt("Enter first nummber: "));
 var number2 = parseInt(prompt("Enter second number: "));
 
-alert(add(number1, number2));
+alert(divide(number1, number2));
